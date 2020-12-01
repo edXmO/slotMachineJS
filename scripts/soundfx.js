cf59b267d43c77a76a689d374ed86f6f44e4f267
Eduardo <ex.romeroma@gmail.com>
2020-12-01 22:47:35 +0100

    Fixed upper sound button, changes img when on/off, turns on and off on click

--- scripts/soundfx.js
+++ scripts/soundfx.js
@@ -1,24 +1,17 @@
-
-// Background music onload
-
-// var audio = document.getElementById("casino-audio");
-
-window.addEventListener("click", () => {
-    document.getElementById("img-speaker").addEventListener('click', soundSwitch(document.getElementById("casino-audio")));
-
-})
-
 // Button turning on/off audio -
-function soundSwitch(music) {
+function soundSwitch() {
     currentValue = document.getElementById('img-speaker').value;
     if (currentValue == "off") {
         document.getElementById('img-speaker').value = "on";
         document.getElementById('img-speaker').src = "/img/speaker-on.png";
-        music.play()
+        document.getElementById("casino-audio").play()
     } else {
         document.getElementById('img-speaker').value = "off";
         document.getElementById('img-speaker').src = "/img/speaker-off.png";
-        music.pause();
+        document.getElementById("casino-audio").pause();
     }
 }
 
+function soundInsertCoin() {
+
+}
